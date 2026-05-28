e1088884576472ef016c7e88597a41fe7e65d8a0
zhinian <zhinian.qin@outlook.com>
2026-05-29 03:56:57 +0800

    Support partial SM70 macro-N qweight offsets

--- csrc/quantization/marlin/sm70_dense_iterator_utils.cuh
+++ csrc/quantization/marlin/sm70_dense_iterator_utils.cuh
@@ -24,16 +24,20 @@ CUTLASS_DEVICE int u4_macro_n_qweight_offset_from_logical(int size_n,
                                                             int logical_n) {
   int const k_tile = logical_k / kQuantTileK;
   int const local_k = logical_k - k_tile * kQuantTileK;
+  int const n_tiles = size_n / kQuantTileN;
   int const n_tile = logical_n / kQuantTileN;
   int const macro_n_tile = n_tile / kMacroNTiles;
   int const macro_first_n_tile = macro_n_tile * kMacroNTiles;
   int const subtile = n_tile - macro_first_n_tile;
+  int const remaining_n_tiles = n_tiles - macro_first_n_tile;
+  int const subtile_count =
+      remaining_n_tiles < kMacroNTiles ? remaining_n_tiles : kMacroNTiles;
   int const local_n_vec = (logical_n - n_tile * kQuantTileN) / 8;
   int const local_word = local_k * (kQuantTileN / 8) + local_n_vec;
 
   return k_tile * (size_n * 2) +
          macro_n_tile * kMacroNTiles * (kQuantTileK * kQuantTileN / 8) +
-         local_word * kMacroNTiles + subtile;
+         local_word * subtile_count + subtile;
 }
 
 CUTLASS_DEVICE int u8_macro_n_qweight_offset_from_logical(int size_n,
@@ -41,16 +45,20 @@ CUTLASS_DEVICE int u8_macro_n_qweight_offset_from_logical(int size_n,
                                                             int logical_n) {
   int const k_tile = logical_k / kQuantTileK;
   int const local_k = logical_k - k_tile * kQuantTileK;
+  int const n_tiles = size_n / kQuantTileN;
   int const n_tile = logical_n / kQuantTileN;
   int const macro_n_tile = n_tile / kMacroNTiles;
   int const macro_first_n_tile = macro_n_tile * kMacroNTiles;
   int const subtile = n_tile - macro_first_n_tile;
+  int const remaining_n_tiles = n_tiles - macro_first_n_tile;
+  int const subtile_count =
+      remaining_n_tiles < kMacroNTiles ? remaining_n_tiles : kMacroNTiles;
   int const local_n_word = (logical_n - n_tile * kQuantTileN) / 4;
   int const local_word = local_k * (kQuantTileN / 4) + local_n_word;
 
   return k_tile * (size_n * 4) +
          macro_n_tile * kMacroNTiles * (kQuantTileK * kQuantTileN / 4) +
-         local_word * kMacroNTiles + subtile;
+         local_word * subtile_count + subtile;
 }
 
 CUTLASS_DEVICE int u8_macro_n_qweight_word_stride_from_logical(
